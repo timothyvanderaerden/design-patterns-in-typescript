b9df3c71852af97e7dfa1ae68b26a310c3ede2c2
Timothy Vanderaerden <timothy.vanderaerden@sippy.cloud>
2020-07-17 12:07:21 +0200

    Update main program
    Use switch statement for ease of use.

--- main.ts
+++ main.ts
@@ -8,11 +8,13 @@ program
   .option('--strategy', 'Demo strategy pattern.')
   .parse(process.argv);
 
-
-if (program.observer) {
-  observer();
-} else if (program.strategy) {
-  strategy();
-} else {
-  console.log('You need to specify a design pattern.');
+switch(true) {
+  case program.observer:
+    observer();
+    break;
+  case program.strategy:
+    strategy();
+    break;
+  default:
+    console.log('You need to specify a design pattern.');
 }
