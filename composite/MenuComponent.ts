2ee0b77b80ab84e83f4ce0627e8475374a9ce2b0
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2020-08-03 13:41:22 +0200

    [ESLint] fix unused vars in abstract class

--- composite/MenuComponent.ts
+++ composite/MenuComponent.ts
@@ -1,12 +1,15 @@
 export abstract class MenuComponent {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public add(menuComponent: MenuComponent): void {
     throw new Error('Unsupported operation');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public remove(menuComponent: MenuComponent): void {
     throw new Error('Unsupported operation');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public getChild(index: number): MenuComponent {
     throw new Error('Unsupported operation');
   }
